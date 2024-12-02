9f3a810749cbb8800f60ad30c00000af30dbd38f
Joosterbooster <joost@gunneman2.nl>
2024-12-02 21:51:00 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+var greetingMsg = "Hello All";
+var heading = document.querySelector("#greeting");
+if (heading) {
+    heading.textContent = greetingMsg;
+}
---
+++ index.ts
@@ -0,0 +1,6 @@
+const greetingMsg: string = "Hello All";
+const heading = document.querySelector("#greeting");
+
+if (heading) {
+  heading.textContent = greetingMsg;
+}
